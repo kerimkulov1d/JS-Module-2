dee994a1c0e9fa7ca1e81d13534b34d8631ba429
Daniyar Kerimkulov <daniyar.kerimkulov.kk@gmail.com>
2022-01-27 23:48:46 +0600

    changed script1

--- Task-2/js/script1.js
+++ Task-2/js/script1.js
@@ -1,5 +1,5 @@
 // 1. Дана строка 'ddd@bbb@ccc'. Замените все @ на '!'
 
 let mixedLetters = 'ddd@bbb@ccc';
-let changeLetters = mixedLetters.replace(/@/g, '!');
+let changeLetters = mixedLetters.replaceAll('@', '!');
 console.log(changeLetters);
