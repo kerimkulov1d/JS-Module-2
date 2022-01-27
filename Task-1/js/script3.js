6f8d82a780b193ad8a562311c80077e754c5bec4
Daniyar Kerimkulov <daniyar.kerimkulov.kk@gmail.com>
2022-01-27 15:53:06 +0600

    finished

---
+++ Task-1/js/script3.js
@@ -0,0 +1,7 @@
+// 3. Напишите несколько строк кода, выводящих на экран ваше имя и почтовый адрес.
+
+let myName = 'Данияр';
+let myEmail = 'daniyar.kerimkulov.kk@gmail.com';
+
+console.log(`Меня зовут ${myName}`);
+console.log(`Мой почтовый адрес ${myEmail}`);
